46abd09b315dd5134034a614b6611d31b7b6c4cf
Alba1020 <59318883+Alba1020@users.noreply.github.com>
2022-11-02 20:24:15 -0400

    create project

---
+++ my.first.project/src/test/java/SanityTests.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class SanityTests {
+  @Test
+  public void canRunTest() {
+  }
+  @Test
+  public void canUseVar() {
+	  String test = "Adrian";
+	  var testOne = "Adrian";
+  }
+}
