08234197955e03f7cabde574d8fb8b31a3e0bd26
Alba1020 <59318883+Alba1020@users.noreply.github.com>
2022-11-05 22:56:33 -0400

    First project work submission.

---
+++ java.exercises/src/test/java/LocationObjectMothers.java
@@ -0,0 +1,20 @@
+public class LocationObjectMothers {
+
+	public static LocationObjectMother NewOrleans() {
+	
+		return new LocationBuilder()
+				.withState(States.Louisiana)
+				.withCity(Cities.NewOrleans)
+				.build();
+	}
+	
+	public static LocationObjectMother Houston() {
+		
+		return new LocationBuilder()
+				.withState(States.Texas)
+				.withCity(Cities.Houston)
+				.withNFLTeam(nflTeams.Texans)
+				.build();
+	}
+}
+
